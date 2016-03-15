6f3d54513f41f9a223e0de52752321a89e96371c
juanes1154 <juanes.1154@live.com>
2016-03-15 07:22:13 -0500

    Commit first exercise

---
+++ ConsoleApp1/Arrays/Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program2
+    {
+
+        static void Main(string[] args)
+        {
+            sbyte Num1, Num2, Result = 0;
+
+            Num1 = Convert.ToSByte(Console.ReadLine());
+            Num2 = Convert.ToSByte(Console.ReadLine());
+
+            for (byte i = 0; i < Math.Abs(Num1); i++)
+                Result = Convert.ToSByte(Result + Num2);
+
+
+            if (Num1 < 0 && Num2 < 0)
+                Result = Math.Abs(Result);
+            if (Num1 < 0 && Num2 > 0)
+                Result = Convert.ToSByte(Result - Result - Result);
+
+            Console.WriteLine("The Result is : " + Result);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[]  A= { 5, 3, 6, 3, 4, 2 };
+            int max = 0, x, y, N = A.Length;
+            string parMaximo = "";
+            for (int i = 0; i < N; i++)
+            {
+               x = A[i];
+               for (int j = 0; j < N; j++)
+                {
+                    y = A[j];
+                    if (i <= j && j < N && x <= y)
+                   {
+                       Console.WriteLine("(" + i + ") (" + j + ")" + " "+ "Distance " + Convert.ToString(j-i));
+                      if ((j - i) > max)
+                     {
+                         parMaximo = "(" + i + ") (" + j + ")";
+                          max = j- i;
+                     }            
+                    }
+                }
+            }
+            Console.WriteLine("The max distance " + max);
+            Console.WriteLine(parMaximo);
+            Console.ReadKey(); 
+        }
+    }
+
+
+}
+
+
